4aee7067dbc3c3cc9320ff170b032ce752dcae33
mtai83 <benguyen08032003@gmail.com>
2024-05-27 02:11:55 +0700

    Init tool GUI

---
+++ GUI.py
@@ -0,0 +1,31 @@
+import netProbe
+import arpSpoof
+import time
+
+def main():
+    while(True):
+
+        print("""  
+  __  __             ___        _   _          __  __ _    _    _ _     
+ |  \/  |__ _ _ _   |_ _|_ _   | |_| |_  ___  |  \/  (_)__| |__| | |___ 
+ | |\/| / _` | ' \   | || ' \  |  _| ' \/ -_) | |\/| | / _` / _` | / -_)
+ |_|  |_\__,_|_||_| |___|_||_|  \__|_||_\___| |_|  |_|_\__,_\__,_|_\___|                                                                                                                            
+              """)
+        print("""
+            author: Nguyễn Minh Tài, Nguyễn Tùng Lâm, Trần Quang Huy
+              
+            ----- DASHBOARD -----
+              1. Network Probe
+              2. ARP Spoof
+              999. Thoát chương trình
+              """)
+        options = int(input("Lựa chọn option:  "))
+        if(options == 1):
+            netProbe.main()
+        elif(options == 2):
+            arpSpoof.main()
+        elif(options == 999):
+            exit()
+
+if __name__ == "__main__":
+    main()
