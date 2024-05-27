686d1dfa76fdafae4ce3a5d722392bc7d31ab54a
mtai83 <benguyen08032003@gmail.com>
2024-05-27 10:03:47 -0400

    Init Feature Control Packet

---
+++ controlPacket.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def main():
+    print("""
+        Do you want forward or intercept packet ? (select option number)
+          Option 1 : Forward packet
+          Option 2 : Intercept packet
+    """)
+    option = int(input("Your choice: "))
+    if(option == 1):
+        try:
+            bash = subprocess.Popen(['/home/kali/Man-in-the-Middle_G60/Man_in_the-Middle/forwardPacket.sh'], shell=True)
+            bash.wait()
+            print("Set up Forward Packet success ! ----")
+        except Exception:
+            print("Please use sudo/root privilege")
+    elif(option == 2):
+        try:
+            bash = subprocess.Popen(['/home/kali/Man-in-the-Middle_G60/Man_in_the-Middle/interceptPacket.sh'], shell=True)
+            bash.wait()
+            print("Set up Intercept Packet success ! ----")
+        except Exception:
+            print("Please use sudo/root privilege")
+    
+if __name__ == "__main__":
+    main()
